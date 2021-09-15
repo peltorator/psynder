a2ec1dd2c47440f7654433c2bcddde3e7014768f
Ivan Pavlov <pavlov200912@gmail.com>
2021-09-15 23:26:47 +0300

    login mocked

--- android/app/src/main/java/com/psinder/myapplication/LoginActivity.kt
+++ android/app/src/main/java/com/psinder/myapplication/LoginActivity.kt
@@ -10,7 +10,20 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton
 import com.psinder.myapplication.databinding.ActivityLoginBinding
+import android.os.AsyncTask
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import android.os.Handler
+import android.widget.EditText
+import android.widget.Toast
+import com.psinder.myapplication.network.LoginData
+import com.psinder.myapplication.network.provideApi
 
 
 class LoginActivity : AppCompatActivity() {
@@ -19,10 +32,43 @@ class LoginActivity : AppCompatActivity() {
         //for changing status bar icon colors
         //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         setContentView(R.layout.activity_login)
+
+        val btn = findViewById<CircularProgressButton>(R.id.cirLoginButton)
+
+//        lifecycleScope.launch {
+//            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                withContext(Dispatchers.IO) {
+//                    Thread.sleep(3000)
+//                    btn.revertAnimation();
+//                }
+//            }
+//        }
+
+
     }
 
-    fun onLoginClick(View: View?) {
+    fun onRegisterClick(View: View?) {
         startActivity(Intent(this, RegisterActivity::class.java))
         overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
+    }
+
+    fun onLoginClick(view: View?) {
+        (view as CircularProgressButton).startAnimation()
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                val nepapka = withContext(Dispatchers.IO) {
+                    //Thread.sleep(3000)
+                    provideApi().login(
+                    LoginData(
+                        "eve.holt@reqres.in",//findViewById<EditText>(R.id.editTextEmail).text.toString(),
+                        "cityslicka" // findViewById<EditText>(R.id.editTextPassword).text.toString()
+                    )
+                    )
+                }
+                println("KEK" + nepapka.token)
+                view.revertAnimation()
+            }
+        }
     }
 }
