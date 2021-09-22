8b7422a903fbd4abfa015419df03834da22fea3d
Ivan Pavlov <pavlov200912@gmail.com>
2021-09-22 23:35:36 +0300

    Activity replaced with fragments & nav_graph added

---
+++ android/app/src/main/java/com/psinder/myapplication/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.psinder.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
